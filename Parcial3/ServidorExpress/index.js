e839f26773afcb854faf8722b1b74a986f67a24d
Franco Martinez Cisneros <franco_dortmund13@live.com.mx>
2023-06-01 07:52:17 -0500

    Updated

--- Parcial3/ServidorExpress/index.js
+++ Parcial3/ServidorExpress/index.js
@@ -1,9 +1,9 @@
-import express, { json } from 'express';
+const express = require('express');
 const app = express();
-import { createConnection } from 'mysql';
-import cors from 'cors';
+const mysql = require('mysql');
+const cors = require('cors');
 
-app.use(json());
+app.use(express.json());
 
 app.use(cors());
 
